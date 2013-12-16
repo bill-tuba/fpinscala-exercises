ea39740ceb41c1f833cd972cf5e3e6e1c43ce15a
billtuba <bill_t_adams@hotmail.com>
2013-12-16 17:22:35 -0600

    now trees

---
+++ src/test/scala/TreeSpec.scala
@@ -0,0 +1,27 @@
+import org.scalatest.{ShouldMatchers, FlatSpec}
+
+class TreeSpec extends FlatSpec with ShouldMatchers {
+
+  "EXERCISE 25:" should """Write a function size that counts the number of nodes
+                              |(leaves and branches) in a tree.""" in {
+    pending
+  }
+
+
+  """EXERCISE 26:""" should """Write a function maximum that returns the maximum element
+                              |in a Tree[Int] . (Note: in Scala, you can use x.max(y) or x max y to
+                              |compute the maximum of two integers x and y .)""" in {
+    pending
+  }
+
+  """EXERCISE 27:""" should """Write a function depth that returns the maximum path length from
+                              |the root of a tree to any leaf.""" in {
+    pending
+  }
+  """EXERCISE 28:""" should """Write a function map , analogous to the method of the same
+                              |name on List , that modifies each element in a tree with a given function.
+                              |""" in {
+    pending
+
+  }
+}
