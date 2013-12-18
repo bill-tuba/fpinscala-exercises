93a317af3b371f9b2b42381bd3d0fa26bea230d2
billtuba <bill_t_adams@hotmail.com>
2013-12-18 09:05:07 -0600

    adding tree tests and refactored adding a foldr

--- src/main/scala/fpinscala/chapter2/Tree.scala
+++ src/main/scala/fpinscala/chapter2/Tree.scala
@@ -1,7 +1,71 @@
 package fpinscala.chapter2
 
 
-sealed trait Tree[+A]
+sealed trait Tree[+A] {
+
+  def size: Int = {
+    def loop(tree: Tree[A], total: Int): Int = tree match {
+      case Nil                 => total
+      case Leaf(_)             => total + 1
+      case Branch(left, right) => 1 + loop(left,total) + loop(right,total)
+    }
+    loop(this, 0)
+  }
+}
+
 case class Leaf[A](value: A) extends Tree[A]
-case class Branch[A](left: Tree[A], right: Tree[A]) extends Tree[A]
+
+case class Branch[+A](left: Tree[A], right: Tree[A]) extends Tree[A]
+
+case object Nil extends Tree[Nothing]
+
+object Tree{
+
+  def head[A](that : Tree[A]) : A = that match {
+    case Leaf(a) => a
+    case Branch(l,_) => head(l)
+  }
+
+  def headLeaf[A](that : Tree[A]) : Tree[A] = that match {
+    case Leaf(a) => Leaf(a)
+    case Branch(l,_) => headLeaf(l)
+  }
+
+  def size2[A](that : Tree[A]): Int = that match {
+      case Leaf(_)             => 1
+      case Branch(left, right) => 1 + size2(left) + size2(right)
+      case _                   => 0
+  }
+
+  def size[A,B](that : Tree[A]): Int = foldr(that)(0)( _ + size(_) + size(_))
+
+  def foldr[A,B](tree : Tree[A])(z : B)(f : (B, Tree[A], Tree[A]) => B): B = tree match{
+    case Branch(left, right) => f(z,left,right)
+    case _                   => z
+  }
+
+  def maximum (tree : Tree[Int]): Int = foldr(tree)(head(tree))((t,l,r)=> maximum(l) max maximum(r))
+
+  def depth[A](t : Tree[A] ) : Int = foldr(t)(0)((t,l,r) => 1 + ( depth(l) max depth(r)))
+  //
+  def map[A,B](t:Tree[A])(f : (A)=>B) : Tree[B]=
+//    fold(t)(0)((t, left, right) =>  Branch(map(left)(f), map(right)(f)))
+    t match {
+    case Leaf(b)             =>  Leaf(f(b))
+    case Branch(left, right) =>  Branch(map(left)(f), map(right)(f))
+    case _ => Nil
+  }
+
+  def fold[A,B](tree : Tree[A])(z:B)(f :( A ) => B) : Tree[B]= tree match{
+    case Leaf(a)             =>  Leaf( f(a) )
+    case Branch(left, right) =>  Branch(fold(left)(z)(f), fold(right)(z)(f))
+    case _ => Nil
+  }
+
+
+
+
+  def apply[A](as: A*): Tree[A] =
+   Nil
+}
 
--- src/test/scala/TreeSpec.scala
+++ src/test/scala/TreeSpec.scala
@@ -1,27 +1,57 @@
+import fpinscala.chapter2.{Tree , Branch, Leaf}
 import org.scalatest.{ShouldMatchers, FlatSpec}
 
 class TreeSpec extends FlatSpec with ShouldMatchers {
 
-  "EXERCISE 25:" should """Write a function size that counts the number of nodes
-                              |(leaves and branches) in a tree.""" in {
-    pending
+  behavior of "A Tree"
+  "EXERCISE 25:" should "Write a function size that counts the number of nodes (leaves and branches) in a tree." in {
+    assert( Nil.size === 0 )
+    assert( Leaf(1).size === 1 )
+    assert( Branch(Leaf(1),Leaf(1)).size === 3)
+    assert( Branch(Leaf(1),Branch(Leaf(1), Leaf(1))).size === 5)
   }
 
 
-  """EXERCISE 26:""" should """Write a function maximum that returns the maximum element
-                              |in a Tree[Int] . (Note: in Scala, you can use x.max(y) or x max y to
-                              |compute the maximum of two integers x and y .)""" in {
-    pending
+  "EXERCISE 26:" should "Write a function maximum that returns the maximum element"+
+                          "in a Tree[Int] . (Note: in Scala, you can use x.max(y) or x max y to"+
+                              "compute the maximum of two integers x and y .)" in {
+
+    assert( Tree.maximum( Leaf(1) ) === 1 )
+    assert(Tree.maximum(Leaf(2)) === 2)
+
+    assert( Tree.maximum( Branch(Leaf(1),Leaf(2))) === 2)
+    assert( Tree.maximum( Branch(Leaf(2),Leaf(1))) === 2)
+    assert( Tree.maximum( Branch(Leaf(1),Branch(Leaf(2), Leaf(3)))) === 3)
+
+    assert( Tree.maximum( Branch(Leaf(1),Branch(Leaf(2), Leaf(3)))) === 3)
+
   }
 
-  """EXERCISE 27:""" should """Write a function depth that returns the maximum path length from
-                              |the root of a tree to any leaf.""" in {
-    pending
+  "EXERCISE 27:" should "Write a function depth that returns the maximum path length from the " +
+                          "root of a tree to any leaf." in {
+
+    assert( Tree.depth(Leaf(0)) === 0 )
+    assert( Tree.depth(Branch(Leaf(0),Leaf(0))) === 1 )
+    assert( Tree.depth( Branch(Leaf(1),Branch(Leaf(2), Leaf(3)))) === 2)
+    assert( Tree.depth( Branch( Branch ( Branch(Leaf(1), Leaf(1)), Leaf(1)),Branch(Leaf(2), Leaf(3)))) === 3)
   }
-  """EXERCISE 28:""" should """Write a function map , analogous to the method of the same
-                              |name on List , that modifies each element in a tree with a given function.
-                              |""" in {
-    pending
+
+  "EXERCISE 28:" should "Write a function map, analogous to the method of the same name on List,"+
+                          "|that modifies each element in a tree with a given function." in {
+
+    assert( Tree.map(Leaf(0)){ _ + 1}  === Leaf(1) )
+    assert( Tree.map( Branch(Leaf(0),Leaf(0) )){ _ + 1}  === Branch(Leaf(1),Leaf(1)))
+    assert( Tree.map( Branch(Leaf(1),Leaf(2) )){ _ + 1}  === Branch(Leaf(2),Leaf(3)))
+    assert( Tree.map( Branch(Leaf(1),Branch(Leaf(2), Leaf(3))) )(_ * 2) === Branch(Leaf(2),Branch(Leaf(4), Leaf(6))))
+
+  }
+  "EXERCISE 29:" should  "Generalize size , maximum , depth , and map , writing a new"+
+                           "function fold that abstracts over their similarities. Re-Implement them in terms of"+
+                           "this more general function. Can you draw an analogy between this fold function"+
+                           "and the left and right folds for List ?"  in {
+
+    assert( Tree.fold(Leaf(1))(0)(_ + 1) === Leaf(2))
+
 
   }
 }
