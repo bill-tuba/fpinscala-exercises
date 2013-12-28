609635ba4048b8c6dc4cf35342de5fc05ccd684d
billtuba <bill_t_adams@hotmail.com>
2013-12-27 22:32:48 -0600

    variations on option HOF impls brain bender without matchers

--- src/main/scala/fpinscala/chapter3/Option.scala
+++ src/main/scala/fpinscala/chapter3/Option.scala
@@ -2,29 +2,41 @@ package fpinscala.chapter3
 
 sealed trait Option[+A] {
 
-  def map[B >: A](f: A => B): Option[B] = this match {
+  def map[B](f: A => B): Option[B] = this match {
     case Some(a) => Some(f(a))
     case _ => None
   }
 
-  def flatMap[B](f : (A => Option[B])) : Option[B] = this match{
-    case Some(a) => f(a)
-    case None => None
-  }
-
-  def filter(f: A => Boolean): Option[A] = this match {
-    case Some(a) if (f(a)) => Some(a)
-    case _ => None
-  }
-
-  def getOrElse[B >: A](default : => B ) : B = this match {
+  def getOrElse[B >: A](default: => B): B = this match {
     case Some(a) => a
     case _ => default
   }
-  def orElse[B >: A] ( default : => Option[B]) : Option[B] = this match {
+
+  def flatMap[B](f: (A => Option[B])): Option[B] = this match {
+    case Some(a) => f(a)
+    case _ => None
+  }
+
+  def flatMap2[B](f: (A => Option[B])): Option[B] = map(f) getOrElse None
+
+  def filter(f: A => Boolean): Option[A] = this match {
+    case Some(a) if (f(a)) => this
+    case _ => None
+  }
+
+  def filter2(f: A => Boolean): Option[A] =  if (map(f) getOrElse false) this else None
+
+  def filter3 (f: A => Boolean): Option[A] =  flatMap(a => if(f(a)) Some(a) else None)
+
+  def orElse[B >: A](default: => Option[B]): Option[B] = this match {
     case None => default
     case _ => this
   }
+
+  def orElse2[B >: A](default: => Option[B]): Option[B]  = if( this ==  None) default else this
+
+  def orElse3[B >: A](default: => Option[B]): Option[B]  = filter( _ ==  None)
+
 }
 
 case class Some[+A](get: A) extends Option[A]
