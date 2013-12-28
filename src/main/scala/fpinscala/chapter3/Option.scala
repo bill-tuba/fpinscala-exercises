5d19d36915739d876806528df95eebfe1823976a
billtuba <bill_t_adams@hotmail.com>
2013-12-27 19:37:07 -0600

    adding option tests map filter orElse getOrElse

--- src/main/scala/fpinscala/chapter3/Option.scala
+++ src/main/scala/fpinscala/chapter3/Option.scala
@@ -1,10 +1,27 @@
 package fpinscala.chapter3
 
-sealed trait Option[+A]
+sealed trait Option[+A] {
 
-case class Some[+A](get : A) extends Option[A]
-case object None extends Option[Nothing]
+  def map[B >: A](f: A => B): Option[B] = this match {
+    case Some(a) => Some(f(a))
+    case _ => None
+  }
 
-object Option {
+  def filter(f: A => Boolean): Option[A] = this match {
+    case Some(a) if (f(a)) => Some(a)
+    case _ => None
+  }
 
+  def getOrElse[B >: A](default : => B ) : B = this match {
+    case Some(a) => a
+    case _ => default
+  }
+  def orElse[B >: A] ( default : => Option[B]) : Option[B] = this match {
+    case None => default
+    case _ => this
+  }
 }
+
+case class Some[+A](get: A) extends Option[A]
+
+case object None extends Option[Nothing]
--- src/test/scala/fpinscala/chapter3/OptionSpec.scala
+++ src/test/scala/fpinscala/chapter3/OptionSpec.scala
@@ -1,6 +1,53 @@
 package fpinscala.chapter3
 
+import org.scalatest.{WordSpec, Matchers}
 
-class OptionSpec {
+
+class OptionSpec extends WordSpec with Matchers{
+
+  "Options" when{
+    "map" should{
+      "apply function on Some" in {
+        Some(1).map( _ + 1) shouldEqual Some(2)
+        Some(2.0).map( _ * 2) shouldEqual Some(4.0)
+        Some("1").map( _ + "x") shouldEqual Some("1x")
+      }
+      "return None on None" in{
+        (None : Option[Int]).map( _ * 2) shouldEqual None
+      }
+    }
+    "filtering" should{
+      "yield None if None" in {
+        (None : Option[Int]).filter( _ == 2) shouldEqual None
+      }
+
+      "yield None if predicate resolves to False" in {
+        Some(1).filter( _ == 2) shouldEqual None
+      }
+
+      "yield Some if predicate resolves to True" in {
+        Some(2).filter( _ ==2 ) shouldEqual Some(2)
+      }
+    }
+
+
+    "Get Or Else" should {
+      "return get-value on Some" in{
+        Some(1).getOrElse(2) shouldEqual 1
+      }
+      "return else-value on None" in{
+        None.getOrElse(2) shouldEqual 2
+      }
+    }
+  }
+
+  "Or Else evaluation" should {
+    "take place for None" in{
+      None.orElse(Some(2)) shouldEqual Some(2)
+    }
+    "not take place for Some" in{
+      Some(1).orElse( Some(2) ) shouldEqual Some(1)
+    }
+  }
 
 }
