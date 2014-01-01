6dcf99efc68d4cbb2c6238c36dd98d9326334716
billtuba <bill_t_adams@hotmail.com>
2013-12-31 21:42:09 -0600

    added sequence fn

--- src/test/scala/fpinscala/chapter3/OptionSpec.scala
+++ src/test/scala/fpinscala/chapter3/OptionSpec.scala
@@ -2,7 +2,6 @@ package fpinscala.chapter3
 
 import org.scalatest.{WordSpec, Matchers}
 
-
 class OptionSpec extends WordSpec with Matchers {
 
   "Options" when {
@@ -59,5 +58,41 @@ class OptionSpec extends WordSpec with Matchers {
     "not take place for Some" in {
       Some(1).orElse(Some(2)) shouldEqual Some(1)
     }
+  }
+  /*
+  EXERCISE 4: Write a function sequence , that combines a list of Options
+into one option containing a list of all the Some values in the original list. If the
+  original list contains None even once, the result of the function should be None ,
+otherwise the result should be Some with a list of all the values. Here is its
+signature:
+
+def sequence[A](a: List[Option[A]]): Option[List[A]]
+
+   */
+  "Exercise 4 ch 3" should {
+    "List with all somes is an Option of Some(List)" in {
+      sequence(List(Some(1))) shouldEqual Some(List(1))
+      sequence(List(Some(1), Some(2))) shouldEqual Some(List(1, 2))
+      sequence(List(Some(1), Some(2), Some(3))) shouldEqual Some(List(1, 2, 3))
+    }
+
+    "List with any None is None" in {
+      sequence(List(None)) shouldEqual None
+      sequence(List(Some(1), None)) shouldEqual None
+      sequence(List(Some(1), None, Some(2))) shouldEqual None
+      sequence(List(Some(1), Some(2), Some(3), None)) shouldEqual None
+      sequence(List(None, Some(1), Some(2), Some(3))) shouldEqual None
+    }
+
+  }
+
+  def sequence[A](a: List[Option[A]]): Option[List[A]] = {
+    //this is probably a fold
+    def loop(list: List[Option[A]], ret: Some[List[A]]): Option[List[A]] = list match {
+      case Some(x) :: rest => loop(rest, Some(ret.get :+ x))
+      case None :: _ => None
+      case Nil => ret
+    }
+    loop(a, Some(List()))
   }
 }
