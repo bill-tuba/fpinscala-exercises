3dbfb6f3bf82fa22fb00d7a4d1e0e546635ca91b
billtuba <bill_t_adams@hotmail.com>
2014-01-04 12:07:23 -0600

    exploring the warnings

--- src/test/scala/fpinscala/chapter5/examples.scala
+++ src/test/scala/fpinscala/chapter5/examples.scala
@@ -2,20 +2,17 @@ package fpinscala.chapter5
 
 import org.scalatest._
 
-/**
- * Created by billa on 1/4/14.
- */
 class examples extends FlatSpec with Matchers {
 
   it should "evaluate only the truthy case" in {
-    assert(if2(true, 3, sys.error("uh oh")) === 3)
+    assert(if2(b = true, 3, sys.error("uh oh")) === 3)
   }
 
   it should "evaluate only the falsey case" in {
     a[RuntimeException] should be thrownBy {
-      if2(false, 3, sys.error("uh oh"))
+      if2(b = false, 3, sys.error("uh oh"))
     }
   }
 
-  def if2[A](boolean: Boolean, onTrue: => A, onFalse: => A) = if (boolean) onTrue else onFalse
+  def if2[A](b: Boolean, onTrue: => A, onFalse: => A) = if (b) onTrue else onFalse
 }
