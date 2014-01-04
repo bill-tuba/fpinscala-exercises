df8d2c4e7cf65ebddae7ebe721f1649a0217475b
billtuba <bill_t_adams@hotmail.com>
2014-01-04 11:58:58 -0600

    refactor / split test methods

--- src/test/scala/fpinscala/chapter5/examples.scala
+++ src/test/scala/fpinscala/chapter5/examples.scala
@@ -7,15 +7,15 @@ import org.scalatest._
  */
 class examples extends FlatSpec with Matchers {
 
-  it should "be a playground" in {
-
+  it should "evaluate only the truthy case" in {
     assert(if2(true, 3, sys.error("uh oh")) === 3)
+  }
+
+  it should "evaluate only the falsey case" in {
     a[RuntimeException] should be thrownBy {
       if2(false, 3, sys.error("uh oh"))
     }
-
   }
 
-  def if2[A](boolean: Boolean, onTrue: => A, onFalse: => A) =
-    if (boolean) onTrue else onFalse
+  def if2[A](boolean: Boolean, onTrue: => A, onFalse: => A) = if (boolean) onTrue else onFalse
 }
